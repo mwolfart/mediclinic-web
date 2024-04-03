7bb843e355116df1668206ba4ee1749cc03edab8
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-04-02 22:43:54 -0300

    Add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import angular from 'angular';
 
+angular.module('myApp', ['ui.bootstrap']);
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
